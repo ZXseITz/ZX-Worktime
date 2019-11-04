0378b37995516f747950982730a409a912dcd116
Claudio Seitz <zxseitz@gmail.com>
2019-11-04 18:51:48 +0100

    impl local file handling, removed mongodb

--- Worktime/MainWindow.xaml.cs
+++ Worktime/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 using MongoDB.Bson;
 using Worktime.src;
 using Worktime.src.data;
@@ -15,71 +13,24 @@ namespace Worktime
     public partial class MainWindow : Window
     {
         private Model _model;
-        private DbHandler _database;
+        private FileHandler<List<Project>> _projectsHandler;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void OnLoaded(object sender, RoutedEventArgs e)
+        private async void OnLoaded(object sender, RoutedEventArgs e)
         {
+            _projectsHandler = new FileHandler<List<Project>>("D:\\OneDrive\\Documents\\Worktime\\projects.json");
             _model = FindResource("Model") as Model;
             if (_model == null) throw new Exception("Cannot find model");
-            _model.Projects.Add(new Project(ObjectId.Empty, "All Projects"));
-            var config = ConfigHandler.Instance;
-            _database = new DbHandler((string)config["host"], (int)config["port"], (string) config["db"],
-                (string)config["projects"], (string) config["workitems"]);
+            _model.Projects.Add(new Project("All Projects"));
 
-            //model.QueryFrom = DateTime.MinValue;
-            //model.QueryTo = DateTime.MaxValue;
+            var projectTask = _projectsHandler.Load();
 
-            //_model.PropertyChanged += (o, args) =>
-            //{
-            //    switch (args.PropertyName)
-            //    {
-            //        case "QueryProject":
-            //            // todo update db query
-            //            break;
-            //        case "QueryFrom":
-            //            // todo update db query
-            //            break;
-            //        case "QueryTo":
-            //            // todo: update db query
-            //            break;
-            //    }
-            //};
-
-            _database.GetProjects().ContinueWith(task => Application.Current?.Dispatcher.InvokeAsync(() =>
-                task.Result.ForEach(_model.Projects.Add)));
-            //_database.GetWorkItems().ContinueWith(task => Application.Current?.Dispatcher.InvokeAsync(() =>
-            //    task.Result.ForEach(_model.WorkItems.Add)));
-        }
-
-        private void OnKeyDown(object sender, KeyEventArgs e)
-        {
-            //todo: remove test shortcut
-            if (e.Key == Key.N && (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)))
-            {
-                CreateProject("Test");
-            }
-        }
-
-        private void CreateProject(string name)
-        {
-            var project = new Project(ObjectId.GenerateNewId(), name);
-            _database.CreateProject(project);
-            _model.Projects.Add(project);
-        }
-
-        private void UpdateProject(Project project)
-        {
-            _database.UpdateProject(project);
-        }
-
-        private void DeleteProject(Project project)
-        {
-            _database.DeleteProject(project);
+            var projects = await projectTask;
+            projects.ForEach(_model.Projects.Add);
         }
     }
 }
--- Worktime/src/data/Project.cs
+++ Worktime/src/data/Project.cs
@@ -1,13 +1,7 @@
-﻿
-
-using MongoDB.Bson;
-
-namespace Worktime.src.data
+﻿namespace Worktime.src.data
 {
     public class Project : AbstractModel
     {
-        public ObjectId Id { get; }
-
         private string _name;
 
         public string Name
@@ -20,9 +14,8 @@ namespace Worktime.src.data
             }
         }
 
-        public Project(ObjectId id, string name)
+        public Project(string name)
         {
-            Id = id;
             _name = name;
         }
     }
